f8201704ff16becd870ee76ddcca7963446eae6b
kevinchewning <klcbusiness@hotmail.com>
2021-09-23 15:44:22 -0400

    Added redirect for HTTPS

--- server.js
+++ server.js
@@ -35,6 +35,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+      if (req.headers['x-forwarded-proto'] !== 'https')
+          return res.redirect('https://' + req.headers.host + req.url);
+      else
+          return next();
+  } else
+      return next();
+});
+
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {

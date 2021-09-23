4a4c5363760b63bb956611ad3ed9e0243dfd3030
kevinchewning <klcbusiness@hotmail.com>
2021-09-23 15:50:46 -0400

    corrected HTTPS redirect

--- server.js
+++ server.js
@@ -36,13 +36,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  if (process.env.NODE_ENV === 'production') {
-      if (req.headers['x-forwarded-proto'] !== 'https')
-          return res.redirect('https://' + req.headers.host + req.url);
-      else
-          return next();
-  } else
-      return next();
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect('https://' + req.headers.host + req.url);
+  } else {
+    return next();
+  }
 });
 
 app.use(routes);
